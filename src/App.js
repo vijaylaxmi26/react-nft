b1565afbb09c163b2fba4843a23c6b98058e9a55
vijaylaxmi verma <vijaylaxmi262001@gmail.com>
2022-02-22 00:03:06 +0530

    collection base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import CollectionCard from "./Components/CollectionCard";
 import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name={"punk"}
+        traits={[{ value: 7 }]}
+        image="https://ipfs.thirdweb.com/ipfs/QmYJXUCPzfiMvNW5NvNtGLVZoVoe1cpWCELyhhjqJpmE5N/0.jpg"
+      />
     </div>
   );
 }
